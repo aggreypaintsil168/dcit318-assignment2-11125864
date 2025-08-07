3aaaf7312819ef90ae946a647dc43cc50fd9679c
Aggrey Paintsil Ishmeal <aggreypaintsil14@gmail.com>
2025-08-07 14:05:55 +0000

    Successfully Implemented Inheritance and Overriding in my Code

---
+++ InOverApp/InOverApp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+// Parent class
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+// Child class
+class Dog : Animal 
+{
+    public override void MakeSound() {
+        Console.WriteLine("Bark");
+    }
+}
+
+// Another child class
+class Cat : Animal 
+{
+    public override void MakeSound() {
+        Console.WriteLine("Meow");
+    }
+}
+
+// Testing
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myAnimal.MakeSound(); // Output: Some generic sound
+        myDog.MakeSound();    // Output: Bark
+        myCat.MakeSound();    // Output: Meow
+    }
+}
